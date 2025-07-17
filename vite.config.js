346a6cf45e9c9b7e7284626fa0a531084da58062
logic-lab-code <156294119+logic-lab-code@users.noreply.github.com>
2025-07-17 09:37:31 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
-
-export default {
   css: {
     modules: {
-      scopeBehaviour: 'local',
+      scopeBehaviour: 'local', // This was originally outside, now included here.
     }
   }
-}
-
+})
