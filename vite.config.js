476f54f0e4129568db46d52dc58fa73bcf5cdec2
logic-lab-code <156294119+logic-lab-code@users.noreply.github.com>
2025-07-17 09:44:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Ensure Vercel looks at the correct output directory
-  },
   css: {
     modules: {
-      scopeBehaviour: 'local',
-    }
-  }
+      scopeBehaviour: 'local', // Make sure this is inside the same export
+    },
+  },
 });
